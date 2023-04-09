e34ef225dc92dfa62165c55fd441ff1d3bd0afb4
Ilia Medvedev <ilia.medvedev@codefresh.io>
2023-04-09 11:49:17 +0300

    add list image for mirror script

---
+++ scripts/list-images-for-mirrror.sh
@@ -0,0 +1,5 @@
+MYDIR=$(dirname $0)
+CHARTDIR="${MYDIR}/../charts/gitops-runtime"
+VALUESFILE="${CHARTDIR}/ci/ingressless-values.yaml"
+#helm dependency update $CHARTDIR > /dev/null
+helm template --values $VALUESFILE $CHARTDIR | grep -E -i  ".*Image: " | awk -F ':' '{if ($2) printf "%s:%s\n", $2, $3}' | tr -d "\"|[:blank:]" | sort -u |uniq -i
