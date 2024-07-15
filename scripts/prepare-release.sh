bdecbf70b1e266e41f213fc81029f82967103b97
mikhail-klimko <mklimko123@gmail.com>
2024-07-15 13:23:31 +0300

    feat: add prepare-release script

---
+++ scripts/prepare-release.sh
@@ -0,0 +1,18 @@
+GITOPS_RUNTIME_REPO=codefresh-io/gitops-runtime-helm
+APP_PROXY_REPO=codefresh-io/argo-platform
+
+docker run \
+    -w "/chart" \
+    -v "$(pwd):/chart" \
+    -e APP_PROXY_REPO=$APP_PROXY_REPO \
+    -e GITOPS_RUNTIME_REPO=$GITOPS_RUNTIME_REPO \
+    -e GITHUB_TOKEN=$(gh auth token) \
+    -u $(id -u) \
+    --rm \
+    quay.io/codefresh/gitops-runtime-scripts /scripts/prepare-release.sh
+
+docker run \
+    -v "$(pwd):/helm-docs" \
+    -u $(id -u) \
+    --rm \
+    jnorwood/helm-docs:v1.9.1
