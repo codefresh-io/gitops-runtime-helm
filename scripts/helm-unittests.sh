6cf1d75c2ca4363094f2796a025d290a9a812e2c
Noam Gal <noam.gal@octopus.com>
2025-10-05 12:42:44 +0300

    fixed tests

--- scripts/helm-unittests.sh
+++ scripts/helm-unittests.sh
@@ -5,4 +5,4 @@ CHART_DIR="$(cd "$(dirname "$0")/.." && pwd)"
 echo "$CHART_DIR"
 
 echo "Running Helm unittests"
-docker run --entrypoint "/bin/sh" -it --rm -v $CHART_DIR/charts:/charts alpine/helm:3.19.0 -c 'helm plugin install https://github.com/helm-unittest/helm-unittest.git --version 1.0.1 && helm unittest /charts/gitops-runtime'
+docker run --entrypoint "/bin/sh" -it --rm -v $CHART_DIR/charts:/charts alpine/helm:3.19.0 -c 'helm plugin install https://github.com/helm-unittest/helm-unittest.git --version 1.0.2 && helm unittest /charts/gitops-runtime'
